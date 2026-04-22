a74a6dd38d02c66971211a33993155d560769d6b
Agustina <agustinagalante65@gmail.com>
2026-04-22 11:22:44 -0300

    correcciones

--- src/main/java/com/uade/tpo/demo/entidades/Producto.java
+++ src/main/java/com/uade/tpo/demo/entidades/Producto.java
@@ -28,21 +28,17 @@ public class Producto {
     @Column
     private String tipo;
 
-    //1. Relación con Categoría (muchos a uno)
     @ManyToOne
     @JoinColumn(name = "id_categoria")
     private Categoria categoria;
 
-    //2. Relación con Vendedor (muchos a uno). El ID_vendedor se mapea a la entidad Usuario.
     @ManyToOne
     @JoinColumn(name = "id_vendedor")
     private Usuario vendedor;
 
-    //3. Relación con imagenes (uno a muchos)
     @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL)
     private List<Imagen> imagenes;
 
-    //4. Relación con materiales (muchos a muchos)
     @ManyToMany
     @JoinTable(
         name = "producto_material",
@@ -51,7 +47,6 @@ public class Producto {
     )
     private List<Material> materiales;
 
-    //5. Relación con descuentos (muchos a muchos)
     @ManyToMany
     @JoinTable(
         name = "producto_descuento",
