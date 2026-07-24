c6e0f90eb65640b119c93cdde9420193fe69691c
frannkyanka <121457189+frannkyanka@users.noreply.github.com>
2026-07-24 01:01:27 -0300

    fix(pedidos): asegurar validacion de precios e implementar control transaccional

--- src/main/java/com/uade/tpo/demo/entidades/dto/SolicitudPedido.java
+++ src/main/java/com/uade/tpo/demo/entidades/dto/SolicitudPedido.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 @Data
 public class SolicitudPedido {
-    private double total;
+    // Se elimina el atributo 'total'
     private String direccionEnvio;
+    private String emailContacto;
+    private String cuponAplicado;
     private List<ItemPedido> productos;
 
     @Data
     public static class ItemPedido {
         private Long idProducto;
-        private String nombreProducto;
-        private double precioUnitario;
         private int cantidad;
+        // Se eliminan precioUnitario y nombreProducto. Se obtienen de la BD.
     }
 }
